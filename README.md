c21e7a6586f07fc77d39342a91b88df7985c64f7
glendilly <glen.goodvin@gmail.com>
2020-04-30 21:39:05 -0700

    make linter happy

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![Build Status](https://travis-ci.org/glendilly/hello-world.svg?branch=master)](https://travis-ci.org/glendilly/hello-world) [![Maintainability](https://api.codeclimate.com/v1/badges/7323957db56e6113df31/maintainability)](https://codeclimate.com/github/glendilly/hello-world/maintainability) [![Test Coverage](https://api.codeclimate.com/v1/badges/7323957db56e6113df31/test_coverage)](https://codeclimate.com/github/glendilly/hello-world/test_coverage)
 
-This project calculates the value of a call option using the Black model.
+This project calculates the value of a call option using the Black-Scholes formula.
--- src/derivatives_maths.py
+++ src/derivatives_maths.py
@@ -1,13 +1,21 @@
+"""
+Derivatives mathematics functions
+"""
+
+
 from math import log, sqrt
 from scipy.stats import norm
 
 
 def black_scholes(forward, strike, time_to_expiry, volatility):
+    """
+    Black-Scholes pricer
+    """
     if forward < 0.0:
         raise Exception("Forward cannot be negative")
     if strike < 0.0:
         raise Exception(" Stike cannot be negative")
-    d1 = (log(forward/strike) +
-          volatility**2*time_to_expiry/2.0)/volatility/sqrt(time_to_expiry)
-    d2 = d1 - volatility*sqrt(time_to_expiry)
-    return forward*norm.cdf(d1) - strike*norm.cdf(d2)
+    d_1 = (log(forward/strike) +
+           volatility**2*time_to_expiry/2.0)/volatility/sqrt(time_to_expiry)
+    d_2 = d_1 - volatility*sqrt(time_to_expiry)
+    return forward*norm.cdf(d_1) - strike*norm.cdf(d_2)
